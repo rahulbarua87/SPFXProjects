0dbffa355e80197bcfa35ffe15b1c9fe90d81db7
Rahul Barua <Rahul.barua87@gmail.com>
2019-06-14 16:36:49 -0400

    New Files

---
+++ GetSpItems/src/webparts/getListItems/GetListItemsWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './GetListItemsWebPart.module.scss';
+import * as strings from 'GetListItemsWebPartStrings';
+
+export interface IGetListItemsWebPartProps {
+  description: string;
+}
+
+export default class GetListItemsWebPart extends BaseClientSideWebPart<IGetListItemsWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.getListItems }">
+        <div class="${ styles.container }">
+          <div class="${ styles.row }">
+            <div class="${ styles.column }">
+              <span class="${ styles.title }">Welcome to SharePoint! Demo</span>
+              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${ styles.description }">${escape(this.properties.description)}</p>
+              <a href="https://aka.ms/spfx" class="${ styles.button }">
+                <span class="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ GetSpItems/src/webparts/getListItems/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGetListItemsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GetListItemsWebPartStrings' {
+  const strings: IGetListItemsWebPartStrings;
+  export = strings;
+}
